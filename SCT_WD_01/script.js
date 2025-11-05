ab5d33ded000b32952d6211e225cabeea6314681
Neha <20p0478mcc@gmail.com>
2025-11-06 00:34:50 +0530

    Initial commit - added landing page project

---
+++ SCT_WD_01/script.js
@@ -0,0 +1,9 @@
+// Change navbar color when scrolling
+window.addEventListener("scroll", () => {
+  const nav = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+});
